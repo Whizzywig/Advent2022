a40bb83290988e6995fae8f94652c8be273226d8
Whizzywig <whizzywig.rockley@gmail.com>
2022-12-01 21:12:21 +0000

    complete task1

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -2,25 +2,59 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("Starting... \n", .{});
+    
+    var general = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = general.deinit();
+    const gpa = general.allocator();
 
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+    
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var current: u32 = 0;
+    var most: u32 = 0;
+    var list = std.ArrayList(u32).init(gpa);
+    defer list.deinit();
+    var tmp: u32 = 0;
+
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        //std.debug.print("line: {s}\n", .{line});
+        if (line.len == 1){
+            var slice = list.toOwnedSlice();
+            defer gpa.free(slice);
+            tmp = total(slice);
+            if (tmp > most){
+                most = tmp;
+            }
+        } else {
+            //std.debug.print("buf len = {d}", .{line.len});
+            current = try std.fmt.parseInt(u32, line[0..(line.len-1)], 10);
+            try list.append(current);
+        }
+    }
+
+    std.debug.print("most: {d}", .{most});
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    // try bw.flush(); // don't forget to flush!
 }
 
-pub fn total(list: []const u64) u64 {
-    var total: u64 = 0;
+pub fn total(list: []const u32) u32 {
+    var output: u32 = 0;
     for (list) |item| {
-        total += item;
+        output += item;
     }
+    return output;
 }
 
 test "simple test" {
@@ -31,6 +65,6 @@ test "simple test" {
 }
 
 test "value adding" {
-    const test_items = [_]u64 {1000,2000,3000};
-    try std.testing.expect(total(test_items) == 6000);
+    const test_items = [_]u32 {1000,2000,3000};
+    try std.testing.expect(total(&test_items) == 6000);
 }
